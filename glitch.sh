870c20316b7a6b08012aa6d887d0c006fa1fc037
textbrowser@gmail.com <textbrowser@gmail.com>
2026-07-24 20:34:02 -0400

    Regular file.

--- glitch.sh
+++ glitch.sh
@@ -4,7 +4,7 @@
 
 set_qt_qpa_platformtheme()
 {
-    qt6=$(ldd "$1" | grep Qt6 2>/dev/null)
+    qt6=$(ldd "$1" 2>/dev/null | grep Qt6 2>/dev/null)
 
     if [ ! -z "$qt6" ]
     then
@@ -22,25 +22,33 @@ export AA_USEHIGHDPIPIXMAPS=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_X11_NO_MITSHM=1
 
-if [ -r ./Glitch ] && [ -x ./Glitch ]
+if [ -f ./Glitch ] && [ -r ./Glitch ] && [ -x ./Glitch ]
 then
     echo "Launching a local Glitch."
     set_qt_qpa_platformtheme "./Glitch"
     ./Glitch "$@"
     exit $?
-elif [ -r /opt/glitch/Glitch ] && [ -x /opt/glitch/Glitch ]
+fi
+
+if [ -f /opt/glitch/Glitch ] && \
+   [ -r /opt/glitch/Glitch ] && \
+   [ -x /opt/glitch/Glitch ]
 then
-    echo "Launching an official Glitch."
+    echo "Launching an official Glitch (/opt/glitch)."
     set_qt_qpa_platformtheme "/opt/glitch/Glitch"
     /opt/glitch/Glitch "$@"
     exit $?
-elif [ -r /usr/local/glitch/Glitch ] && [ -x /usr/local/glitch/Glitch ]
+fi
+
+if [ -f /usr/local/glitch/Glitch ] && \
+   [ -r /usr/local/glitch/Glitch ] && \
+   [ -x /usr/local/glitch/Glitch ]
 then
-    echo "Launching an official Glitch."
+    echo "Launching an official Glitch (/usr/local/glitch)."
     set_qt_qpa_platformtheme "/usr/local/glitch/Glitch"
     /usr/local/glitch/Glitch "$@"
     exit $?
-else
-    echo "Cannot locate Glitch. Why?"
-    exit 1
 fi
+
+echo "Cannot locate Glitch. Why?"
+exit 1
